54cc31678245c315f30d89c697aa987ffdae7ae9
loribeiro <l.oribeiro@outlook.com>
2020-08-26 01:14:41 -0300

    changing suspense fallback

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,19 @@
 import React,{Suspense,lazy} from 'react';
 /* import { ApolloProvider } from 'react-apollo';
 import { client, client2 } from './settings'; */
+//import HomePage from "./components/pages/HomePage"
+import {LoadingPageLite} from "./GeneralComponents"
 const HomePage= lazy(()=>import("./components/pages/HomePage")) 
 const LoginPage= lazy(()=>import("./components/pages/LoginPage")) 
 const SubscriptionPage= lazy(()=>import("./components/pages/SubscriptionPage"))
 const AppPage= lazy(()=>import("./components/pages/AppPage")) 
 const JoinPage= lazy(()=>import("./components/pages/JoinHome")) 
 const Routes = {
-    "/": () =><Suspense fallback={<div></div>}> <HomePage/></Suspense>,//{/* <Suspense fallback = {<LoadingPageLite/>}><HomePage/></Suspense> , */}
-    "/login": () =><Suspense fallback={<div></div>}><LoginPage/></Suspense> ,
-    "/cadastro": () => <Suspense fallback={<div></div>}><SubscriptionPage/></Suspense> ,
-    "/app": () => <Suspense fallback={<div></div>}><AppPage/></Suspense> ,
-    "/join": () => <Suspense fallback={<div></div>}><JoinPage/> </Suspense> ,
+    "/": () => <Suspense fallback = {<LoadingPageLite/>}><HomePage/></Suspense>,//{/* <Suspense fallback = {<LoadingPageLite/>}><HomePage/></Suspense> , */}
+    "/login": () =><Suspense fallback={<LoadingPageLite/>}><LoginPage/></Suspense> ,
+    "/cadastro": () => <Suspense fallback={<LoadingPageLite/>}><SubscriptionPage/></Suspense> ,
+    "/app": () => <Suspense fallback={<LoadingPageLite/>}><AppPage/></Suspense> ,
+    "/join": () => <Suspense fallback={<LoadingPageLite/>}><JoinPage/> </Suspense> ,
     
 };
 
