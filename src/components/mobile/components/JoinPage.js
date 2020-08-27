1c157f0523739f2980141eb9db0cc833f4120b48
loribeiro <l.oribeiro@outlook.com>
2020-08-26 22:00:55 -0300

    updating the components to work mobile and desktop

--- src/components/mobile/components/JoinPage.js
+++ src/components/mobile/components/JoinPage.js
@@ -1,53 +1,96 @@
 import React, { useEffect, useState } from "react";
-import { Input,Button } from 'antd';
+import { Input,Button,Spin } from 'antd';
 import { client2 } from "../../../settings";
-import { ApolloProvider, useMutation } from "@apollo/react-hooks";
 import { Create_Home } from '../../../Queries';
+import { isMobile} from 'react-device-detect';
+import { gql, useMutation} from '@apollo/client';
+import {LoadingPageLite} from "../../../GeneralComponents"
 
 export const CreatingHouse = (props) =>{
     const [nome, setNome] = useState("")
+    const[CreateHome,
+        {loading: mutationLoading, error: mutationError, data}, ] = useMutation(Create_Home);
     const handleCreation = () =>{
         
         if(nome!==""){
-
-            client2.mutate({
-                mutation: Create_Home,
-                variables:{nome:nome}
-            }).then(
-                (response)=>{
-                    console.log(response)
-                    window.location.href = "/app"
-
-                }
-            ).catch(err =>{})
+            CreateHome({ variables:{nome:nome}}).catch(err => {})
+            
         }
     }
-    return(
-        <div style={{marginTop:"10vh"}}>
+    if(data){
+        
+        window.location.href = "/app"
+    }
+    if(mutationLoading){
+        return(<div style={{textAlign:"center"}}>
+        <h1><strong>Quase lá :D</strong></h1>
+        <Spin size="small" />
+    </div>)
+    }
+    if(mutationError){
+        return(
+            <div>Algo deu errado, atualize a pagina e tente novamente</div>
+        )
+    }
+    if(isMobile){
 
-            <h1>Crie o seu lar</h1>
+        return(
+            <div style={{marginTop:"10vh"}}>
+    
+                <h1>Crie o seu lar</h1>
+                <div style={{display:"grid", gridTemplateColumns:"0.95fr 0.05fr"}}>
+                    <div>
+                        <Input onChange = {value=>{setNome(value.target.value)}} name ="casa" placeholder="Nome da Casa" />
+                    </div>
+                    <div>
+                        <Button onClick = {handleCreation} type="primary">Criar Casa</Button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    return(
+        <div style={{marginTop:"5vh", textAlign:"center"}}>
+
+            <h1 style={{color:"white"}}>Crie o seu lar</h1>
             <div style={{display:"grid", gridTemplateColumns:"0.95fr 0.05fr"}}>
                 <div>
                     <Input onChange = {value=>{setNome(value.target.value)}} name ="casa" placeholder="Nome da Casa" />
                 </div>
                 <div>
-                    <Button onClick = {handleCreation} type="primary">Criar Casa</Button>
+                    <Button onClick = {handleCreation} type="danger">Criar Casa</Button>
                 </div>
             </div>
         </div>
     )
 }
 export const JoingHouse = (props) =>{
+    if(isMobile){
+        return(
+            <div style={{marginTop:"5vh"}}>
+    
+                <h1 >Entre em um lar já existente</h1>
+                <div style={{display:"grid", gridTemplateColumns:"0.95fr 0.05fr"}}>
+                    <div>
+                        <Input placeholder="Codigo da Casa" />
+                    </div>
+                    <div>
+                        <Button type="primary">Entrar em uma Casa</Button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return(
-        <div style={{marginTop:"5vh"}}>
+        <div style={{marginTop:"5vh", textAlign:"center"}}>
 
-            <h1>Entre em um lar já existente</h1>
+            <h1 style={{color:"white"}}>Entre em um lar já existente</h1>
             <div style={{display:"grid", gridTemplateColumns:"0.95fr 0.05fr"}}>
                 <div>
                     <Input placeholder="Codigo da Casa" />
                 </div>
                 <div>
-                    <Button type="primary">Entrar em uma Casa</Button>
+                    <Button type="danger">Entrar em uma Casa</Button>
                 </div>
             </div>
         </div>
