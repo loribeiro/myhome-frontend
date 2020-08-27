db5954b2188618e97d8242cef8dad36238e48d0a
loribeiro <l.oribeiro@outlook.com>
2020-08-26 22:03:39 -0300

    adding content to desktop version

--- src/components/desktop/JoinHome.js
+++ src/components/desktop/JoinHome.js
@@ -1,8 +1,23 @@
 import React,{Suspense,lazy} from 'react';
-
+import { Layout } from 'antd';
+import {TopBar} from './components/TopBar'
+import {CreatingHouse,JoingHouse} from '../mobile/components/JoinPage'
+const { Header, Sider, Content,Footer } = Layout;
 const JoinHomeDesktop = (props) =>{
     return(
-        null
+        <Layout style={{}}>
+        <TopBar/>
+        <Content className = "Login" style={{minHeight:"98vh"}}>
+            
+            <div style ={{marginTop:"25vh",marginLeft:"2vw", marginRight:"5vw",maxWidth:"50vw", textAlign:"center"}}>
+                <h1 style={{color:"white"}}>Ainda não tem um lar para chamar de seu?</h1>
+                <h1 style={{color:"white"}}>Peça para o administrador da sua casa um codigo ou crie uma casa agora mesmo!</h1>
+                <CreatingHouse/>
+                <JoingHouse/>
+            </div>
+        </Content>
+        <Footer style={{textAlign:"center"}}>myHome</Footer>
+    </Layout>
     )
 }
 export default JoinHomeDesktop;
