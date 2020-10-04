105c025b06087de33f393633e7345f4c8810a9af
loribeiro <l.oribeiro@outlook.com>
2020-10-03 22:22:14 -0300

    adding accept and make invitation mutations

--- src/Queries.js
+++ src/Queries.js
@@ -1,5 +1,32 @@
 import gql from 'graphql-tag';
 
+export const Accept_ivitation = gql `
+mutation acceptInvitation($password: String!,$token: String!, $sexo: String!,$nome: String!, $sobrenome: String!){
+  acceptInvitation(nome:$nome, sobrenome:$sobrenome, password:$password, sexo:$sexo,token:$token){
+   Pessoa{
+     login{
+       lastName
+     }
+     larUser{
+       id
+     }
+   }
+ }
+ }
+`;
+
+export const Make_Invitation = gql `
+mutation makeInvitation($email: String!, $nome: String!, $sobrenome: String!){
+  makeInvitation(email:$email,nome:$nome, sobrenome:$sobrenome){
+    Lar{
+      organizationInvites{
+        guid
+      }
+    }
+  }
+}
+`;
+
 export const Create_Person= gql`
 mutation criarPessoa($nome: String!, $sobrenome: String!, $email: String! , $sexo: String!, $senha:String!){
   createPessoa(nome:$nome,sobrenome:$sobrenome, email:$email, senha: $senha,sexo:$sexo){
@@ -70,9 +97,12 @@ query{
       restricoesAlimentares
     }
     lar{
-      nome
-      pessoaSet{
-        id
+      name
+      slug
+      organizationUsers{
+        
+          pessoaSet{
+            id
         login{
           id
           firstName
@@ -93,13 +123,15 @@ query{
           plano
           restricoesAlimentares
         }
+          }
+        
       }
       tarefasSet{
         id,
         tarefa,
         responsavel{
           id
-          login{
+          user{
             id
             firstName
             lastName
