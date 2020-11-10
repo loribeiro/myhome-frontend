3d639a7cfbf8345f08e9af77efe0ac03af43e769
Lucas Ribeiro <loribeiro@MacbookPros-MacBook-Pro.local>
2020-11-09 22:17:35 -0300

    turn person admin and change ownership of home

--- src/components/desktop/components/AppPage.js
+++ src/components/desktop/components/AppPage.js
@@ -4,10 +4,9 @@ import {Drawer,Switch,Popconfirm ,Form,Divider ,Input, Button, Space ,AutoComple
 import { MinusCircleOutlined, PlusOutlined,ExclamationCircleOutlined  } from '@ant-design/icons';
 import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import {isMobile} from 'react-device-detect';
-import { client2 } from "../../../settings";
 import { useDispatch, useSelector } from 'react-redux';
-import { gql, useMutation } from '@apollo/client';
-import {Create_Categoria_Conta,Create_Conta,Remove_User_Home,Make_Invitation,Create_Task,Delete_Tarefas,Create_Bem,Create_Contato,Update_Saude} from "../../../Queries"
+import { gql, useMutation, useQuery } from '@apollo/client';
+import {Turn_Adm,Change_Owner,Is_Owner,Create_Categoria_Conta,Create_Conta,Remove_User_Home,Make_Invitation,Create_Task,Delete_Tarefas,Create_Bem,Create_Contato,Update_Saude} from "../../../Queries"
 import { updateSaude } from '../../../store/reducers/personal_data_reducers';
 import Sider from 'antd/lib/layout/Sider';
 import { OmitProps } from 'antd/lib/transfer/ListBody';
@@ -25,6 +24,7 @@ import {
   WhatsappShareButton,
   WhatsappIcon
 } from "react-share";
+import { LoadingPageLite } from '../../../GeneralComponents';
 
 const { TabPane } = Tabs;
 
@@ -289,9 +289,16 @@ const QrCodePresenter = (props) =>{
 const Detalhes = (props)=>{
   const storage = useSelector(state => state)
   const [removeUser, { loading: mutationLoading, error: mutationError,data }] = useMutation(Remove_User_Home);
+  const [addAdmin, { loading: mutationLoading2, error: mutationError2,data: mutationData2 }] = useMutation(Turn_Adm);
+  const { loading, error, data: data2} = useQuery(Is_Owner,{variables:{larId: storage.person.lar.id}});
   const {refetch} = props
-  if(data){
+  if(data || data2){
     refetch()
+  }
+  if(mutationLoading2|| mutationLoading){
+    return(
+      <LoadingPageLite/>
+    )
   }
   if(props.info.login.firstName !== null){
     
@@ -321,6 +328,22 @@ const Detalhes = (props)=>{
           </div>
           :
           null
+        }
+        {
+          data2
+          ?
+          ((data2.isOwner && (props.info.larUser.find(u => u.organization.id === storage.person.lar.id ).isAdmin=== false)) 
+            ?
+            <div style={{textAlign:"center"}}>
+               <br></br>
+            <Button type="primary" onClick = {()=>{addAdmin({variables:{larId: storage.person.lar.id , admId: props.info.larUser.find(u => u.organization.id === storage.person.lar.id ).id }})}}>
+              Tornar Administrador
+            </Button>
+            </div>
+            :
+            null)
+            :
+            null
         }
       </div>
     )
@@ -854,7 +877,11 @@ const AddContatosEmergencia = (props) => {
 
 export const Configuracoes = (props) =>{
   const storage = useSelector(state => state)
-  const [removeUser, { loading: mutationLoading, error: mutationError,data }] = useMutation(Remove_User_Home);
+  const { loading, error, data} = useQuery(Is_Owner,{variables:{larId: storage.person.lar.id}});
+  
+  const [removeUser, { loading: mutationLoading, error: mutationError,data: mutationData }] = useMutation(Remove_User_Home);
+  const [changeOwner, { loading: mutationLoading2, error: mutationError2,data: mutationData2 }] = useMutation(Change_Owner);
+
   const {refetch} = props
   function showDeleteConfirm() {
     confirm({
@@ -872,14 +899,74 @@ export const Configuracoes = (props) =>{
       },
     });
   }
-  if(data){
+  function showChangeOwnerConfirm() {
+    confirm({
+      title: 'Transferir propriedade do lar para outro usuario',
+      icon: <ExclamationCircleOutlined />,
+      content: <div>
+
+        Apenas um administrador poderá gerer um novo convite após esta ação.
+        {storage.person.moradores.map((info)=>(
+              
+              <div key={info[0].login.id} style={{display:'grid', gridTemplateColumns:"1fr 1fr"}}>
+                <h3 key={info[0].login.id}>{info[0].login.firstName} {info[0].login.lastName}</h3>
+                <Button size="small" style={{maxWidth:"80px"}} type="primary" onClick={()=>{
+                  changeOwner({variables:{larId:storage.person.lar.id, newId:  info[0].larUser.find(u => u.organization.id === storage.person.lar.id ).id}})
+                }} shape="round">Escolher</Button>
+                
+                
+              </div>
+           
+              
+                //info.map(m=> <h1>{m.id}</h1>)
+            )
+                
+            )}
+      </div>
+      ,
+    
+      okText: "Cancelar",
+      okType: 'danger',
+      cancelText: 'Cancelar',
+      onOk() {
+          //removeUser({variables:{email:storage.person.personal.email, larId:storage.person.lar.id}})
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
+  if(mutationLoading){
+    return(
+      <LoadingPageLite/>
+    )
+  }
+  if(mutationData || mutationData2){
     window.location.reload(false);
   }
+ 
   return(
     <div>
         <Button type="danger" onClick = {showDeleteConfirm}>
           Sair deste lar permanentemente
       </Button>
+      {
+        data
+        ?
+        (data.isOwner
+        ?
+        <div>
+           <br></br>
+        <Button type="primary" onClick = {showChangeOwnerConfirm}>
+          Transferir propriedade do lar para outro usuario
+        </Button>
+        </div>
+        :
+        null)
+        :
+        null
+
+      }
     </div>
   )
 }
